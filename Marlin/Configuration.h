0bc424f7dfffea71ad3063ec978a5c0f502c882e
lozprocter <72624353+lozprocter@users.noreply.github.com>
2022-01-13 16:28:27 +0000

    Preheat settings changed. Label: ABS/PETG > PETG. Bed temp: 80 > 75

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1836,9 +1836,9 @@ All modifications over the original configuration file (from Marlin distribution
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS/PETG"
+#define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    80 //Customized DBP
+#define PREHEAT_2_TEMP_BED    75 //Customized DBP
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
